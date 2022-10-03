33c791608150d44103ccda00a9b45ace705ba5e0
annony <ltn2057@protonmail.com>
2022-10-03 10:02:16 -0400

    ship factory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-export { capitalize }
+export { Ship }
 
-function capitalize(s) {
-  return s.toUpperCase()
+function Ship(length) {
+  return {
+    length,
+    isHit: Array(length),
+    hit(n) {
+      this.isHit[n] = true
+    },
+    isSunk() {
+      for (let element of this.isHit) {
+        if (element != true) return false
+      }
+      return true
+    },
+  }
 }
+let ship = Ship(5)
+console.log(ship.isSunk())
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,5 +1,10 @@
-import { capitalize } from ".."
+import { Ship } from ".."
 
-test("capitalize", () => {
-  expect(capitalize("something")).toBe("SOMETHING")
+test("Ship Factory", () => {
+  let ship = Ship(5)
+  expect(Ship(5).length).toBe(5)
+  expect(Ship(5).isHit.length).toBe(5)
+  expect(Ship(5).isSunk()).toBe(false)
+  ship.hit(0)
+  expect(ship.isHit[0]).toBe(true)
 })
