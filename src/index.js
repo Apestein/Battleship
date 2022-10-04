80f41d08fb0c502c119eeaad8bbd4f5b02f84d4b
annony <ltn2057@protonmail.com>
2022-10-04 03:29:12 -0400

    Gameboard factoryfunctions

--- src/index.js
+++ src/index.js
@@ -1,19 +1,69 @@
-export { Ship }
+export { Ship, Gameboard }
 
 function Ship(length) {
   return {
     length,
-    isHit: Array(length),
-    hit(n) {
-      this.isHit[n] = true
+    isHit: 0,
+    hit() {
+      this.isHit++
     },
     isSunk() {
-      for (let element of this.isHit) {
-        if (element != true) return false
-      }
-      return true
+      if (this.isHit == length) return true
+      return false
     },
   }
 }
-let ship = Ship(5)
-console.log(ship.isSunk())
+
+function Gameboard() {
+  const board = []
+  for (let i = 0; i < 10; i++) board[i] = Array(10)
+
+  const carrier = Ship(5)
+  const battleship = Ship(4)
+  const cruiser = Ship(3)
+  const submarine = Ship(3)
+  const destroyer = Ship(2)
+
+  function placeShip(x, y, ship) {
+    for (let i = 0; i < ship.length; i++) {
+      if (y + i > 10) return
+      this.board[x][y + i] = ship
+    }
+  }
+
+  function receiveAttack(x, y) {
+    if (this.board[x][y]) {
+      this.board[x][y].hit()
+      return true
+    }
+    return false
+  }
+
+  function gameOver() {
+    if (
+      this.carrier.isSunk() &&
+      this.battleship.isSunk() &&
+      this.cruiser.isSunk() &&
+      this.submarine.isSunk() &&
+      this.destroyer.isSunk()
+    )
+      return true
+    return false
+  }
+
+  return {
+    board,
+    carrier,
+    battleship,
+    cruiser,
+    submarine,
+    destroyer,
+    placeShip,
+    receiveAttack,
+    gameOver,
+  }
+}
+
+let gameboard = Gameboard()
+gameboard.placeShip(0, 0, 5)
+console.log(gameboard.board[0][0])
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,10 +1,27 @@
-import { Ship } from ".."
+import { Ship, Gameboard, gameOver } from ".."
 
 test("Ship Factory", () => {
   let ship = Ship(5)
-  expect(Ship(5).length).toBe(5)
-  expect(Ship(5).isHit.length).toBe(5)
-  expect(Ship(5).isSunk()).toBe(false)
-  ship.hit(0)
-  expect(ship.isHit[0]).toBe(true)
+  expect(ship.isSunk()).toBe(false)
+  ship.hit()
+  expect(ship.isHit).toBe(1)
+})
+
+test("Gameboard Factory", () => {
+  let gameboard = Gameboard()
+  let x = 0
+  let y = 0
+  let ship = Ship(5)
+  gameboard.placeShip(x, y, ship)
+  for (let i = 0; i < ship.length; i++) {
+    expect(gameboard.board[x][y + i]).toBeTruthy()
+  }
+  expect(gameboard.receiveAttack(x, y)).toBe(true)
+  expect(gameboard.receiveAttack(9, 9)).toBe(false)
+  gameboard.carrier.isHit = 5
+  gameboard.battleship.isHit = 4
+  gameboard.cruiser.isHit = 3
+  gameboard.submarine.isHit = 3
+  gameboard.destroyer.isHit = 2
+  expect(gameboard.gameOver()).toBe(true)
 })
